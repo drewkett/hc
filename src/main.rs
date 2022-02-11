55ec41e26887a2897eea232b30b571372c61d69e
Andrew Burkett <burkett.andrew@gmail.com>
2022-02-11 10:26:05 -0800

    Fix spacing on help message

--- src/main.rs
+++ src/main.rs
@@ -71,16 +71,16 @@ fn print_help() {
 hcp [--hcp-id HCP_ID] [--hcp-tee] [--hcp-ignore-code] [cmd [args...]]
 
     HCP_ID can be set using an environment variable
-    --hcp-id HCP_ID    Sets the healthchecks id. This can also be set using the
-                     environment variable HCP_ID
+    --hcp-id HCP_ID   Sets the healthchecks id. This can also be set using the
+                      environment variable HCP_ID
     --hcp-ignore-code Ignore the return code from cmd. Also available using HCP_IGNORE_CODE
     --hcp-tee         Controls whether to also output the cmd stdout/stderr to the local
-                     stdout/stderr. By default the output from the cmd will only get
-                     passed as text to healthchecks. This option can also be enabled
-                     using the environment variable HCP_TEE. Only the existance of the
-                     variable is checked
-    [cmd [args...]]  If no command is passed, the healthcheck will be notified as a
-                     success with the text 'No command given'
+                      stdout/stderr. By default the output from the cmd will only get
+                      passed as text to healthchecks. This option can also be enabled
+                      using the environment variable HCP_TEE. Only the existance of the
+                      variable is checked
+    [cmd [args...]]   If no command is passed, the healthcheck will be notified as a
+                      success with the text 'No command given'
 "
     )
 }
